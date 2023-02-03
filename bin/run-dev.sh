d49b7123e943269dcc01355d58e66af24da745bd
Hazel Koehler <acomputerdog@gmail.com>
2023-02-03 00:41:58 -0500

    Improve output of run-dev script

--- bin/run-dev.sh
+++ bin/run-dev.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-
 export RAILS_ENV=development
 
 # Compile assets so that webpack will work
+echo [run-dev] Precompiling assets...
 bundle exec rails assets:precompile
+
+echo [run-dev] Clearing cache...
 bin/tootctl cache clear
 
+echo [run-dev] Starting foreman...
 foreman start
