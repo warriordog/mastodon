b4cfb0c77cbb099bca8fd4ae29f54736d7cadf78
Hazel Koehler <acomputerdog@gmail.com>
2023-02-09 12:00:49 -0500

    Optimize concurrency in run-dev.sh / run-tests.sh

--- bin/run-dev.sh
+++ bin/run-dev.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
+
 export RAILS_ENV=development
+export NODE_ENV=development
+
+# Optimize concurrency for development mode
+export STREAMING_CLUSTER_NUM=1
+export WEB_CONCURRENCY=1
+export MAX_THREADS=1
+export SIDEKIQ_CONCURRENCY=1
+export DB_POOL=10
 
 # Compile assets so that webpack will work
-echo [run-dev] Precompiling assets...
+echo "[run-dev] Precompiling assets..."
 bundle exec rails assets:precompile
 
-echo [run-dev] Clearing cache...
+echo "[run-dev] Clearing cache..."
 bin/tootctl cache clear
 
-echo [run-dev] Starting foreman...
+echo "[run-dev] Starting foreman..."
 foreman start
--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
 export RAILS_ENV=test
+export NODE_ENV=test
+
+# Optimize concurrency for testing mode
+export STREAMING_CLUSTER_NUM=1
+export WEB_CONCURRENCY=1
+export MAX_THREADS=2
+export SIDEKIQ_CONCURRENCY=2
+export DB_POOL=15
 
 # Compile assets to fix test errors
 # https://stackoverflow.com/questions/52639256/webpackermanifestmissingentryerror
-bundle exec rails assets:precompile
+echo "[run-tests] Precompiling assets..."
+bundle exec rails assets:
+
+echo "[run-tests] Clearing cache..."
 bin/tootctl cache clear
 
-# Run tests
+echo "[run-tests] Starting tests..."
 rspec
