da2ad30b0a34909bb57971f606b7f0077d14ed34
Hazel Koehler <acomputerdog@gmail.com>
2023-06-14 15:28:01 -0400

    increase webpacker output

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -48,7 +48,7 @@ module.exports = merge(sharedConfig, {
     overlay: settings.dev_server.overlay,
     stats: {
       entrypoints: false,
-      errorDetails: false,
+      errorDetails: true,
       modules: false,
       moduleTrace: false,
     },
